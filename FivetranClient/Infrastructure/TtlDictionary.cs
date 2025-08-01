a9625e59039ee5aaf9c142d1e5eefd48b4ced3d9
Szymon Scholz <raspherdev@pm.me>
2025-08-01 22:11:25 +0200

    improve readability and fix nullability

--- FivetranClient/Infrastructure/TtlDictionary.cs
+++ FivetranClient/Infrastructure/TtlDictionary.cs
@@ -1,16 +1,18 @@
-﻿namespace FivetranClient.Infrastructure;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace FivetranClient.Infrastructure;
 
 public class TtlDictionary<TKey, TValue> where TKey : notnull
 {
-    private readonly Dictionary<TKey, (TValue, DateTime)> _dictionary = new();
+    private readonly Dictionary<TKey, (TValue Value, DateTime ExpirationTime)> _dictionary = new();
 
     public TValue GetOrAdd(TKey key, Func<TValue> valueFactory, TimeSpan ttl)
     {
         if (_dictionary.TryGetValue(key, out var entry))
         {
-            if (DateTime.UtcNow < entry.Item2)
+            if (entry.ExpirationTime > DateTime.UtcNow)
             {
-                return entry.Item1;
+                return entry.Value;
             }
 
             _dictionary.Remove(key);
@@ -21,15 +23,15 @@ public class TtlDictionary<TKey, TValue> where TKey : notnull
         return value;
     }
 
-    public bool TryGetValue(TKey key, out TValue value)
+    public bool TryGetValue(TKey key, [NotNullWhen(true)] out TValue? value)
     {
-        if (_dictionary.TryGetValue(key, out var entry) && DateTime.UtcNow < entry.Item2)
+        if (_dictionary.TryGetValue(key, out var entry) && entry.ExpirationTime > DateTime.UtcNow)
         {
-            value = entry.Item1;
-            return true;
+            value = entry.Value;
+            return value is not null;
         }
 
-        value = default!;
+        value = default;
         return false;
     }
 }
