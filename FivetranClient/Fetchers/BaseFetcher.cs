47bdb6118e3dd42ad664d4c9aa6a7da1d22db0bd
Szymon Scholz <raspherdev@pm.me>
2025-08-01 19:20:36 +0200

    redundant type specification

--- FivetranClient/Fetchers/BaseFetcher.cs
+++ FivetranClient/Fetchers/BaseFetcher.cs
@@ -5,7 +5,7 @@ namespace FivetranClient.Fetchers;
 public abstract class BaseFetcher(HttpRequestHandler requestHandler)
 {
     protected readonly HttpRequestHandler RequestHandler = requestHandler;
-    protected static readonly JsonSerializerOptions SerializerOptions = new JsonSerializerOptions
+    protected static readonly JsonSerializerOptions SerializerOptions = new()
     {
         PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower,
         PropertyNameCaseInsensitive = true,
